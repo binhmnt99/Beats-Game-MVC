09593845142d56b6ec789ffa5c1ba4ddf62cfd2b
Mai Nguyen Thanh Binh <76925857+binhmnt99@users.noreply.github.com>
2023-10-05 14:04:21 +0700

    Separating the data

--- Assets/BeatsGame/Scripts/Track.cs
+++ Assets/BeatsGame/Scripts/Track.cs
@@ -1,13 +1,21 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Beats
 {
+    [Serializable]
+    public class TrackData
+    {
+        [Header("Playback Settings")]
+        [Tooltip ("# of beat per minute")]
+        [Range (30, 360)] public int bpm = 120;
+        public List<int> beats;
+    }
+
     public class Track : MonoBehaviour
     {
-        public int bpm = 120;
-        public List<int> beats;
+        public TrackData trackData;
     }
 }
 
