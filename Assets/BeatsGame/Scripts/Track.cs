cbc63a069f2494fa6f9b4c1685bce9593755f03c
Mai Nguyen Thanh Binh <76925857+binhmnt99@users.noreply.github.com>
2023-10-05 11:27:13 +0700

    Shaping the track data(Model)

---
+++ Assets/BeatsGame/Scripts/Track.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Beats
+{
+    public class Track : MonoBehaviour
+    {
+        public int bpm = 120;
+        public List<int> beats;
+    }
+}
+
